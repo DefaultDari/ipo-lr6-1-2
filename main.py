38497fd4b427d95038ddec77f2d115c5f2d6ab8f
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-11-20 20:42:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,23 @@
 # 3. Выводит сумму всех четных элементов
 import random # Импорт библиотеки рандома
 values = [-3, -5, -2, -12, 0, 15, 4, 7, 2] # Список значений для заполнения матрицы
-while True:
-    rows = int(input("Введите количество строк (от 4 до 8): "))
-    cols = int(input("Введите количество столбцов (от 4 до 8): "))
-    if 4 <= rows <= 8 and 4 <= cols <= 8:
-        break
-    else:
-        print("Пожалуйста, введите числа в диапазоне от 4 до 8.")
-matrix = [[random.choice(values) for _ in range(cols)] for _ in range(rows)] # Создание и заполнение матрицы случайными значениями из списка
+rows = random.randint(4, 8)
+cols = random.randint(4, 8)
+# Создание и заполнение матрицы случайными значениями из списка
+matrix = [] 
+for _ in range(rows):
+    row = []
+    for _ in range(cols):
+        row.append(random.choice(values))
+    matrix.append(row) 
+
 print("Сгенерированная матрица:") # Вывод матрицы в форматированном виде
 for row in matrix:
-    print(" ".join(f"{element:4}" for element in row)) # Выводит матрицу в форматированном виде
-even_sum = sum(element for row in matrix for element in row if element % 2 == 0) # Проверка на четность и подсчет суммы четных чисел
+    print(row)
+# Проверка на четность и подсчет суммы четных чисел
+even_sum = 0
+for row in matrix:
+ for element in row:
+    if element % 2 == 0:
+        even_sum+=element 
 print("Сумма всех чётных элементов:", even_sum) # Вывод 
